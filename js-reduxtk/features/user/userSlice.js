23dbb5e4ac004116087234ff886a5a7757730614
Felix Owolabi <owolabifelix78@gmail.com>
2023-06-20 11:36:52 +0100

    js-redux-toolkit

---
+++ js-reduxtk/features/user/userSlice.js
@@ -0,0 +1,22 @@
+const createSlice = require('@reduxjs/toolkit').createSlice
+const createAsyncThunk = require('@reduxjs/toolkit').createAsyncThunk
+
+const initialState = {
+    loading : false,
+    users : [],
+    error : ''
+}
+
+const fetchUsers =  createAsyncThunk('user/fetchUsers',()=>{
+        axios.get('https://jsonplaceholder.typicode.com/users')
+        .then((response)=>{
+            response.data.map((user) => user.id)
+        })
+})
+
+
+const userSlice = createSlice({
+    name:'user',
+    initialState,
+
+})
