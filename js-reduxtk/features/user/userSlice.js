298b86bc688ab829065dbbb229807090b05aaff9
Felix Owolabi <owolabifelix78@gmail.com>
2023-06-20 11:55:16 +0100

    js-redux-toolkit

--- js-reduxtk/features/user/userSlice.js
+++ js-reduxtk/features/user/userSlice.js
@@ -7,6 +7,7 @@ const initialState = {
     error : ''
 }
 
+// Generates pending, fulfilled and rejected action types
 const fetchUsers =  createAsyncThunk('user/fetchUsers',()=>{
         axios.get('https://jsonplaceholder.typicode.com/users')
         .then((response)=>{
@@ -18,5 +19,23 @@ const fetchUsers =  createAsyncThunk('user/fetchUsers',()=>{
 const userSlice = createSlice({
     name:'user',
     initialState,
+    extraReducers : builder =>{
+        builder.addCase(fetchUsers.pending , state=>{
+            state.loading = true
+        } )
+        builder.addCase(fetchUsers.fulfilled, (state,action)=>{
+            state.loading = false
+            state.users = action.payload
+            state.error = ''
+        })
+        builder.addCase(fetchUsers.rejected, (state,action)=>{
+            state.loading = false
+            state.users = []
+            state.error = error.message
+        })
+    }
 
 })
+
+module.exports = userSlice.reducer
+module.exports.fetchUsers = fetchUsers
