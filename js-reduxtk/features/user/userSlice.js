3de07bb253429fd96ad9332a65376336eec935ba
Felix Owolabi <owolabifelix78@gmail.com>
2023-06-20 13:42:26 +0100

    js-redux-toolkit

--- js-reduxtk/features/user/userSlice.js
+++ js-reduxtk/features/user/userSlice.js
@@ -1,28 +1,28 @@
 const createSlice = require('@reduxjs/toolkit').createSlice
 const createAsyncThunk = require('@reduxjs/toolkit').createAsyncThunk
+const axios = require('axios');
 
 const initialState = {
     loading : false,
     users : [],
-    error : ''
+    error : '',
 }
 
 // Generates pending, fulfilled and rejected action types
-const fetchUsers =  createAsyncThunk('user/fetchUsers',()=>{
-        axios.get('https://jsonplaceholder.typicode.com/users')
-        .then((response)=>{
-            response.data.map((user) => user.id)
-        })
+const fetchUsers = createAsyncThunk('user/fetchUsers',()=>{
+    return axios
+        .get('https://jsonplaceholder.typicode.com/users')
+        .then((response)=> response.data.map((user) => user.id) )
 })
 
 
 const userSlice = createSlice({
     name:'user',
     initialState,
-    extraReducers : builder =>{
-        builder.addCase(fetchUsers.pending , state=>{
+    extraReducers : (builder) =>{
+        builder.addCase(fetchUsers.pending , (state)=>{
             state.loading = true
-        } )
+        })
         builder.addCase(fetchUsers.fulfilled, (state,action)=>{
             state.loading = false
             state.users = action.payload
@@ -31,7 +31,7 @@ const userSlice = createSlice({
         builder.addCase(fetchUsers.rejected, (state,action)=>{
             state.loading = false
             state.users = []
-            state.error = error.message
+            state.error = action.error.message
         })
     }
 
--- js-reduxtk/index.js
+++ js-reduxtk/index.js
@@ -1,17 +1,21 @@
   const store = require('./app/store');
   const cakeActions = require('./features/cake/cakeSlice').cakeActions
   const iceCreamActions = require('./features/iceCream/iceCreamSlice').iceCreamActions
+  const fetchUsers = require('./features/user/userSlice').fetchUsers
 
 console.log('Initial State:', store.getState())
-const unsubscribe = store.subscribe(()=>{})
+const unsubscribe = store.subscribe(()=>{
+    console.log('Updated State:', store.getState())
+})
 
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.restocked(5))
-store.dispatch(iceCreamActions.ordered())
-store.dispatch(iceCreamActions.ordered())
-store.dispatch(iceCreamActions.restocked(9))
+store.dispatch(fetchUsers())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.restocked(5))
+// store.dispatch(iceCreamActions.ordered())
+// store.dispatch(iceCreamActions.ordered())
+// store.dispatch(iceCreamActions.restocked(9))
 
-unsubscribe();
+// unsubscribe();
 
